737da4efba8503e93498c113b0b9e0df800a0d7a
gimlichael <michael@geekle.io>
2024-01-07 06:29:29 +0100

    Helper extension.

---
+++ src/Wish.Shared/TaskExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Wish.Shared
+{
+	public static class TaskExtensions
+	{
+		public static async Task<List<T>> ToListAsync<T>(this Task<IEnumerable<T>> source)
+		{
+			var presult = await source.ConfigureAwait(false);
+			return presult.ToList();
+		}
+	}
+}
